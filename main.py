ccdbc0882a5df434c349d892b09f0c6877794b18
BossAHPGaming <aaravhpatel210@gmail.com>
2024-11-25 18:39:32 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+first_name = "Aarav"
+last_name = "Patel"
+full_name = first_name+last_name
+example = "Haa"*5
+
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name :", full_name)
+print("String Multiplied 5 times gives this result :", example)
+
+word = "Coding"
+print("Lenght of String :", len(word))
+print("First letter of String :", word[0])
+print("Last letter of String :", word[5])
